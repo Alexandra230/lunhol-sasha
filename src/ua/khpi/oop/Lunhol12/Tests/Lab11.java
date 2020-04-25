4982cfe2f0ebc3ffeb51038ad5d6806990e89b09
Alexandra230 <55556088+Alexandra230@users.noreply.github.com>
2020-04-25 20:44:11 +0300

    Done

---
+++ src/ua/khpi/oop/Lunhol12/Tests/Lab11.java
@@ -0,0 +1,17 @@
+package ua.khpi.oop.Lunhol12.Tests;
+
+import java.io.IOException;
+
+public class Lab11 {
+	public static void main(String[] args) throws IOException {
+		 if (args.length != 0) {
+	            if (args[0].equals("-auto")) {
+	                UI.auto();
+	            } else {
+	                System.out.println("Repeat entered with params -auto");
+	            }
+	        } else {
+	            UI.menu();
+	        }
+	    }
+}
